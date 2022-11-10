0333c262bc1c449254ddd2e281010588e45e4a00
iremgumustas <117925698+iremgumustas@users.noreply.github.com>
2022-11-10 18:46:50 +0300

    Add files via upload

---
+++ eğitim1.py
@@ -0,0 +1,21 @@
+sayi1=13
+sayi2=34
+print(sayi1)
+print(sayi2)
+toplam= sayi1+sayi2
+print(toplam)
+
+
+string1="13"
+string2="34"
+toplam = string1+string2
+print(toplam)
+
+
+sayi1 = input("Sayiyi giriniz:")
+sayi2 = input("sayi2 yi girer misin:")
+
+sayi1 = int(sayi1)
+sayi2 = int(sayi2)
+toplam = sayi1 + sayi2
+print(toplam)
